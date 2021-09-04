655f8b069a55209b11aedaefc2ba1e9fe383d5ec
Aleksei Dikun <aldi0221@netcracker.com>
2021-09-04 21:53:14 +0400

    killer feature

--- rosatom-be/src/main/java/ru/sstu/rosatom/service/NalogService.java
+++ rosatom-be/src/main/java/ru/sstu/rosatom/service/NalogService.java
@@ -31,10 +31,13 @@ public class NalogService {
         map.add("page", "1");
         //map.add("query", "");
         //map.add("dtCategory", "10.08.2021");
-        //String buisnessFild = okpd2.split("\\.")[0];
         map.add("okved1", okpd2);
         //map.add("tru", okpd2);
         map.add("region", region);
+        map.add("category", "1");
+        map.add("category", "2");
+        map.add("category", "3");
+        map.add("hasContracts", "1");
         //map.add("slvd", "");
         map.add("pageSize", "100");
         map.add("sortField", "NAME_EX");
